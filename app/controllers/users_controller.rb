1575133513abbf9015cacafc80341f09a509183c
Virginia <vcooper_us@yahoo.com>
2016-01-07 13:07:04 -0800

    Added image gems.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :avatar)
+   params.require(:user).permit(:name, :avatar)
   end
 end
