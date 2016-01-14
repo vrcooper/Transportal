ddb2a1686d9408119abb910a0618376235754814
Virginia <vcooper_us@yahoo.com>
2016-01-14 14:15:35 -0800

    fog updated.

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,5 +1,6 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
+    region: "us-east-1",
     provider:               'AWS',
     aws_access_key_id:       ENV['AWS_ACCESS_KEY_ID'],
     aws_secret_access_key:   ENV['AWS_SECRET_ACCESS_KEY']
