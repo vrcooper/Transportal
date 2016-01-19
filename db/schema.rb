dc4866c672e2853389893d97fe420808e6c984fc
Virginia <vcooper_us@yahoo.com>
2016-01-19 00:53:26 -0800

    deleted migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160119074401) do
+ActiveRecord::Schema.define(version: 20160117084747) do
 
   create_table "collaborations", force: :cascade do |t|
     t.integer  "project_id"
